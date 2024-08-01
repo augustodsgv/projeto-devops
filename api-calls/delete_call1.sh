0b8e3bf521ad2d664d417a6f0861393666255451
augustodsgv <augustodsgv@gmail.com>
2024-08-01 03:32:50 -0300

    Dockerfile: support for using local repository for storing docker images

--- api-calls/delete_call1.sh
+++ api-calls/delete_call1.sh
@@ -1,1 +1,1 @@
-curl -X DELETE localhost:7000/delete_video --json '{"video_name":"batata.mp5"}' -H "Content-Type: application/json"
+curl -X DELETE localhost:7000/delete_video --json '{"video_name":"batata.mp4"}' -H "Content-Type: application/json"
